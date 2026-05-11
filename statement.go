c9ae1213b7d6764ff7057db4f01072655e3ffedd
Belen Cerrotti <bcerrotti@fi.uba.ar>
2026-05-10 23:52:38 -0300

    adding statements

---
+++ statement.go
@@ -0,0 +1,52 @@
+package main
+
+type Stmt interface {
+	stmtNode()
+}
+
+type ExpressionStmt struct {
+	expr Expr
+}
+
+type PrintStmt struct {
+	expr Expr
+}
+
+type VarDecl struct {
+	name  token
+	value Expr // puede ser nil
+}
+
+type BlockStmt struct {
+	statements []Stmt
+}
+
+type IfStmt struct {
+	condition  Expr
+	thenBranch Stmt
+	elseBranch Stmt // puede ser nil
+}
+
+type WhileStmt struct {
+	condition Expr
+	body      Stmt
+}
+
+type FunDecl struct {
+	name       token
+	parameters []token
+	body       []Stmt
+}
+
+type ReturnStmt struct {
+	value Expr
+} // value puede ser nil
+
+func (e *ExpressionStmt) stmtNode() {}
+func (p *PrintStmt) stmtNode()      {}
+func (v *VarDecl) stmtNode()        {}
+func (b *BlockStmt) stmtNode()      {}
+func (i *IfStmt) stmtNode()         {}
+func (w *WhileStmt) stmtNode()      {}
+func (f *FunDecl) stmtNode()        {}
+func (r *ReturnStmt) stmtNode()     {}
