701cadff07a2a9b456d213921302a580538a74dd
Belen Cerrotti <bcerrotti@fi.uba.ar>
2026-05-10 23:55:49 -0300

    fiixing parser

--- parser.go
+++ parser.go
@@ -1,23 +1,44 @@
 package main
 
 type parser struct {
-	tokens []token
+	tokens  []token
+	current int
+}
+
+func (p *parser) currentToken() token {
+	return p.tokens[p.current]
+}
+
+func (p *parser) isAtEnd() bool {
+	return p.current >= len(p.tokens) || p.currentToken().tokenType == EOF
 }
 
 func createParser() *parser {
 	return &parser{
-		tokens: []token{},
+		tokens:  []token{},
+		current: 0,
 	}
 }
 
-func (p *parser) parse(tokens []token) ([]Expr, error) {
+func (p *parser) parse(tokens []token) ([]Stmt, error) {
 	p.tokens = tokens
+	p.current = 0
+	var statements []Stmt
 
-	if len(p.tokens) == 0 {
-		return nil, nil
+	for !p.isAtEnd() {
+		stmt, err := p.getStatement()
+		if err != nil {
+			return nil, err
+		}
+		statements = append(statements, stmt)
 	}
 
-	var expressions []Expr
+	return statements, nil
+}
 
-	return expressions, nil
+func (p *parser) getStatement() (Stmt, error) {
+	var statement Stmt
+
+	return statement, nil
+
 }
