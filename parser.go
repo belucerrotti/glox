08e9b893058ee577e75f410c6464bad7b37278c2
Belen Cerrotti <bcerrotti@fi.uba.ar>
2026-05-12 02:28:42 -0300

    fixes

--- parser.go
+++ parser.go
@@ -39,6 +39,7 @@ func (p *parser) parse() ([]Stmt, error) {
 
 func (p *parser) matches(tokenType TokenType) bool {
 	if !p.isAtEnd() && p.tokens[p.current].tokenType == tokenType {
+		p.current++
 		return true
 	}
 	return false
@@ -74,20 +75,29 @@ func (p *parser) getStatement() (Stmt, error) {
 }
 
 func (p *parser) returnStatement() (Stmt, error) {
-	if p.isAtEnd() {
-		return nil, fmt.Errorf("line %d: expected return value but found '%s'", p.currentToken().line, p.currentToken().value)
+	if p.matches(SEMICOLON) {
+		return &ReturnStmt{}, nil
 	}
-	return p.expressionStatement()
+
+	expr, err := p.parseExpression()
+
+	if err != nil {
+		return nil, err
+	}
+
+	if !p.matches(SEMICOLON) {
+		return nil, fmt.Errorf("line %d: expected ';' but found '%s'", p.currentToken().line, p.currentToken().value)
+	}
+
+	return &ReturnStmt{value: expr}, nil
 }
 
 func (p *parser) funDeclaration() (Stmt, error) {
-	println("entré a fun")
 	funName := p.currentToken()
+
 	if !p.matches(IDENTIFIER) {
 		return nil, fmt.Errorf("line %d: expected IDENTIFIER but found '%s'", p.currentToken().line, p.currentToken().value)
 	}
-
-	println("la funName es " + funName.value)
 
 	if !p.matches(LEFT_PAREN) {
 		return nil, fmt.Errorf("line %d: expected '(' but found '%s'", p.currentToken().line, p.currentToken().value)
@@ -97,71 +107,55 @@ func (p *parser) funDeclaration() (Stmt, error) {
 
 	for !p.isAtEnd() && p.matches(IDENTIFIER) {
 		parameters = append(parameters, p.tokens[p.current-1])
-
-		if p.matches(COMMA) {
-			if !p.matches(IDENTIFIER) {
-				return nil, fmt.Errorf("line %d: expected IDENTIFIER but found '%s'", p.currentToken().line, p.currentToken().value)
-			}
-			continue
+		if !p.matches(COMMA) {
+			break
 		}
-	}
-
-	println("parameters:")
-	for i := 0; i < len(parameters); i++ {
-		println(parameters[i].value)
 	}
 
 	if !p.matches(RIGHT_PAREN) {
 		return nil, fmt.Errorf("line %d: expected ')' but found '%s'", p.currentToken().line, p.currentToken().value)
 	}
 
-	body := []Stmt{}
-
 	if !p.matches(LEFT_BRACE) {
 		return nil, fmt.Errorf("line %d: expected '{' but found '%s'", p.currentToken().line, p.currentToken().value)
-	} else {
-		for !p.isAtEnd() && !p.matches(RIGHT_BRACE) {
-			stmt, err := p.getStatement()
-			if err != nil {
-				return nil, err
-			}
-			body = append(body, stmt)
-		}
+	}
 
-		if !p.matches(RIGHT_BRACE) && p.isAtEnd() {
-			return nil, fmt.Errorf("line %d: expected '}' but found '%s'", p.currentToken().line, p.currentToken().value)
+	body := []Stmt{}
+	for !p.isAtEnd() && !p.matches(RIGHT_BRACE) {
+		stmt, err := p.getStatement()
+		if err != nil {
+			return nil, err
 		}
-
+		body = append(body, stmt)
 	}
 
 	return &FunDecl{name: funName, parameters: parameters, body: body}, nil
 }
 
 func (p *parser) varDeclaration() (Stmt, error) {
-	p.current++ // consumo el 'var'
-
 	name := p.currentToken()
+
 	if !p.matches(IDENTIFIER) {
 		return nil, fmt.Errorf("line %d: expected IDENTIFIER but found '%s'", p.currentToken().line, p.currentToken().value)
-	} else {
-		if p.matches(EQUALS) {
-			expr, err := p.parseExpression()
-			if err != nil {
-				return nil, err
-			}
-			return &VarDecl{name: name, value: expr}, nil
+	}
+
+	var value Expr
+	if p.matches(EQUALS) {
+		expr, err := p.parseExpression()
+		if err != nil {
+			return nil, err
 		}
+		value = expr
 	}
 
 	if !p.matches(SEMICOLON) {
 		return nil, fmt.Errorf("line %d: expected ';' but found '%s'", p.currentToken().line, p.currentToken().value)
 	}
 
-	return &VarDecl{name: name}, nil
+	return &VarDecl{name: name, value: value}, nil
 }
 
 func (p *parser) printStatement() (Stmt, error) {
-	p.current++ // consumo el 'print'
 	expr, err := p.parseExpression()
 	if err != nil {
 		return nil, err
@@ -169,7 +163,6 @@ func (p *parser) printStatement() (Stmt, error) {
 
 	if !p.matches(SEMICOLON) {
 		return nil, fmt.Errorf("line %d: expected ';' but found '%s'", p.currentToken().line, p.currentToken().value)
-	} else {
 	}
 
 	return &PrintStmt{expr: expr}, nil
@@ -183,7 +176,6 @@ func (p *parser) expressionStatement() (Stmt, error) {
 
 	if !p.matches(SEMICOLON) {
 		return nil, fmt.Errorf("line %d: expected ';' but found '%s'", p.currentToken().line, p.currentToken().value)
-	} else {
 	}
 
 	return &ExpressionStmt{expr: expr}, nil
@@ -200,30 +192,34 @@ func (p *parser) parseEquality() (Expr, error) {
 	if err != nil {
 		return nil, err
 	}
+
 	for p.matches(EQUALS_EQUALS) || p.matches(NOTEQUAL) {
-		operator := p.tokens[p.current]
+		operator := p.tokens[p.current-1]
 		right, err := p.parseComparison()
 		if err != nil {
 			return nil, err
 		}
 		left = &BinaryExpr{left: left, operator: operator, right: right}
 	}
+
 	return left, nil
 }
 
 // comparison → term ( ( ">" | ">=" | "<" | "<=" ) term )*
 func (p *parser) parseComparison() (Expr, error) {
 	left, err := p.parseTerm()
+
 	if err != nil {
 		return nil, err
 	}
-	if p.matches(GREATER_THAN) || p.matches(GREATER_EQUALS) || p.matches(LESS_THAN) || p.matches(LESS_EQUALS) {
-		operator := p.tokens[p.current]
+
+	for p.matches(GREATER_THAN) || p.matches(GREATER_EQUALS) || p.matches(LESS_THAN) || p.matches(LESS_EQUALS) {
+		operator := p.tokens[p.current-1]
 		right, err := p.parseTerm()
 		if err != nil {
 			return nil, err
 		}
-		return &BinaryExpr{left: left, operator: operator, right: right}, nil
+		left = &BinaryExpr{left: left, operator: operator, right: right}
 	}
 
 	return left, nil
@@ -235,13 +231,14 @@ func (p *parser) parseTerm() (Expr, error) {
 	if err != nil {
 		return nil, err
 	}
-	if p.matches(PLUS) || p.matches(MINUS) {
-		operator := p.tokens[p.current]
+
+	for p.matches(PLUS) || p.matches(MINUS) {
+		operator := p.tokens[p.current-1]
 		right, err := p.parseFactor()
 		if err != nil {
 			return nil, err
 		}
-		return &BinaryExpr{left: left, operator: operator, right: right}, nil
+		left = &BinaryExpr{left: left, operator: operator, right: right}
 	}
 
 	return left, nil
@@ -253,13 +250,14 @@ func (p *parser) parseFactor() (Expr, error) {
 	if err != nil {
 		return nil, err
 	}
-	if p.matches(STAR) || p.matches(DIVIDE) {
-		operator := p.tokens[p.current]
+
+	for p.matches(STAR) || p.matches(DIVIDE) {
+		operator := p.tokens[p.current-1]
 		right, err := p.parseUnary()
 		if err != nil {
 			return nil, err
 		}
-		return &BinaryExpr{left: left, operator: operator, right: right}, nil
+		left = &BinaryExpr{left: left, operator: operator, right: right}
 	}
 
 	return left, nil
@@ -268,8 +266,8 @@ func (p *parser) parseFactor() (Expr, error) {
 // unary → ( "!" | "-" ) unary | primary
 func (p *parser) parseUnary() (Expr, error) {
 	if p.matches(BANG) || p.matches(MINUS) {
-		operator := p.tokens[p.current]
-		right, err := p.parsePrimary()
+		operator := p.tokens[p.current-1]
+		right, err := p.parseUnary()
 		if err != nil {
 			return nil, err
 		}
@@ -285,6 +283,11 @@ func (p *parser) parsePrimary() (Expr, error) {
 		return &LiteralExpr{value: p.tokens[p.current-1]}, nil
 	}
 
+	if p.matches(IDENTIFIER) {
+		return &LiteralExpr{value: p.tokens[p.current-1]}, nil
+	}
+
 	// todo: grouping "(expression)"
-	return nil, nil
+
+	return nil, fmt.Errorf("line %d: expected expression but found '%s'", p.currentToken().line, p.currentToken().value)
 }
