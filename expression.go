e92db4fa5663df766282bcd4b0fc72b32c0af0d2
Belen Cerrotti <bcerrotti@fi.uba.ar>
2026-05-14 20:14:33 -0300

    adding examples

--- expression.go
+++ expression.go
@@ -5,6 +5,7 @@ type Expr interface {
 }
 
 // expression operator expression
+// ej: 1 + 2, "hola" == "chau", 3 * (4 - 1)
 type BinaryExpr struct {
 	left     Expr
 	operator token
@@ -12,38 +13,48 @@ type BinaryExpr struct {
 }
 
 // operator expression
+// ej: -5, !true
 type UnaryExpr struct {
 	operator token
 	right    Expr
 }
 
 // ( expression )
+// ej: (1 + 2)
 type GroupingExpr struct {
 	expression Expr
 }
 
 // number | string | true | false | nil
+// ej: 42, "hola", true
 type LiteralExpr struct {
 	value token
 }
 
 // and/or (si el left ya define, no se lee el right)
+// ej: (3 > 5) and (4 < 2)
 type LogicalExpr struct {
 	left     Expr
 	operator token
 	right    Expr
 }
 
+// llamada a funcion
+// ej: suma(1, 2)
 type CallExpr struct {
 	callee    Expr
 	paren     token
 	arguments []Expr
 }
 
+// lectura de variable
+// ej: x, miVariable
 type VariableExpr struct {
 	name token
 }
 
+// asignacion de variable
+// ej: x = 5, nombre = "lox"
 type AssignExpr struct {
 	name  token
 	value Expr
